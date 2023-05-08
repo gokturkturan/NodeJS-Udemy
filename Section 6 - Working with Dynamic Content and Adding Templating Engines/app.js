e3bc572be67389e0ce97fece9967b32211106fc8
gokturkturan <113911139+gokturkturan@users.noreply.github.com>
2023-05-08 19:09:01 +0300

    Section 6 - Working with Dynamic Content and Adding Templating Engines

---
+++ Section 6 - Working with Dynamic Content and Adding Templating Engines/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const rootDir = require("./util/path");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+
+app.listen(3000);
